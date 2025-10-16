b49e0895af22290f5d60ff39f2d98b0ff8f54685
Sebastian Mena <menasibajasebastian@gmail.com>
2025-10-16 17:24:24 -0600

    Aprendiendo python hoy un poco
    types que casi no se suelen usar pero para probar
    
    y un poco de variables solo el inicio

---
+++ Variables.py
@@ -0,0 +1,12 @@
+# Variables
+
+my_string_variable = "My string variable"
+print(my_string_variable)
+
+my_int_variable = 4
+print(my_int_variable)
+
+my_bool_variable = False
+print(my_bool_variable)
+
+print(my_string_variable, my_int_variable, my_bool_variable)
---
+++ hello.py
@@ -0,0 +1,8 @@
+print("Hola Mundo")
+
+#Consultar el tipo de dato
+print(type("Soy un dato STR"))
+print(type(34))
+print(type(45.6))
+print(type(4 + 7j))
+print(type([1,2,3]))
