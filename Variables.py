91d09a35ea48dbf5730f8dd98b53b71626f9c561
Sebastian Mena <menasibajasebastian@gmail.com>
2025-11-03 15:07:41 -0600

    Dia 1
    Terminamos variables

--- Variables.py
+++ Variables.py
@@ -10,3 +10,15 @@ my_bool_variable = False
 print(my_bool_variable)
 
 print(my_string_variable, my_int_variable, my_bool_variable)
+
+# Variables en uba sola linea\
+
+name, surname, alias, age = "Sebastian", "Sibaja", "Cabal", 20
+
+print("Me llamo:", name, surname, "Mi edad es de:", age , "Y mi alias es:", alias)
+
+Primer_Nombre = input('Cual es su primer Nombre?:')
+age = input('Que edad tienes?:')
+
+print("El primer nombre del cliente es: ",Primer_Nombre)
+print("La edad seria de: ",age)
